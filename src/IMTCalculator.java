24ecfb381dcc5dceeea3f7f4e13a4123d3504053
Hilmi353H <155254000+Hilmi353H@users.noreply.github.com>
2024-01-12 07:49:09 +0700

    Update IMTCalculator.java
    updateProgram

--- src/IMTCalculator.java
+++ src/IMTCalculator.java
@@ -16,9 +16,46 @@ public class IMTCalculator extends JFrame {
     private JTextField textFieldWeight;
     private JComboBox<String> comboBoxGender;
     private JButton buttonCalculate;
+    private IMTCategoryPanel categoryPanel;
+
+    private class IMTCategoryPanel extends JPanel {
+
+        private String category;
+        private Color color;
+
+        public IMTCategoryPanel(String category, Color color) {
+            this.category = category;
+            this.color = color;
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            Graphics2D g2d = (Graphics2D) g;
+            int width = getWidth();
+            int height = getHeight();
+
+            int diameter = Math.min(width = 84 , height = 84); // Menggunakan diameter yang lebih kecil
+
+            // Menggambar setengah lingkaran dengan diameter yang lebih kecil
+            g2d.setColor(color);
+            g2d.fillArc(0, height - diameter, diameter, diameter, 0, 180);
+
+            // Menambahkan teks kategori
+            g2d.setColor(Color.WHITE);
+            g2d.drawString(category, diameter / 4, height - diameter / 2);
+
+            // Menambahkan panah
+            int arrowSize = 10;
+            int[] xPoints = {diameter / 2, diameter / 2 + arrowSize, diameter / 2 - arrowSize};
+            int[] yPoints = {height - diameter / 2, height - diameter / 2 + arrowSize, height - diameter / 2 + arrowSize};
+            g2d.fillPolygon(xPoints, yPoints, 3);
+        }
+    }
 
     public IMTCalculator() {
-        setLayout(new FlowLayout());
+        setLayout(new BorderLayout());
         Box box = Box.createVerticalBox();
         setTitle("Kalkulator IMT");
         setSize(600, 500);
@@ -31,6 +68,17 @@ public class IMTCalculator extends JFrame {
         labelIMTResult = new JLabel("Nilai IMT: ");
         labelBBResult = new JLabel("Kategori Bobot: ");
 
+        JLabel labelWeightCategories = new JLabel("<html>Indeks Kategori IMT<br>"
+                + "Bobot Terlalu Rendah \t\t&lt;= 15.9<br>"
+                + "Sangat Kekurangan Bobot 16.0-16.9<br>"
+                + "Kurang Bobot \t\t\t17.0-18.4<br>"
+                + "Normal \t\t\t\t18.5-24.9<br>"
+                + "Kelebihan Bobot \t\t25.0-29.9<br>"
+                + "Obesitas kelas I \t\t30.0-34.9<br>"
+                + "Obesitas Kelas II \t\t35.0-39.9<br>"
+                + "Obesitas Kelas III \t\t>=40.0<br>"
+                + "</html>");
+
         textFieldAge = new JTextField(10);
         textFieldHeight = new JTextField(10);
         textFieldWeight = new JTextField(10);
@@ -40,6 +88,10 @@ public class IMTCalculator extends JFrame {
         comboBoxGender.addItem("Wanita");
 
         buttonCalculate = new JButton("Hitung");
+
+        // Inisialisasi panel untuk grafik
+        categoryPanel = new IMTCategoryPanel("", Color.ORANGE);
+
         buttonCalculate.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -54,13 +106,13 @@ public class IMTCalculator extends JFrame {
                 if (imt <= 15.9) {
                     bbResult = "Bobot terlalu rendah";
                 } else if (imt <= 16.9) {
-                    bbResult = "sangat kurang berboobot";
+                    bbResult = "Sangat kekurangan bobot";
                 } else if (imt <= 18.4) {
-                    bbResult = "kurang bobot";
+                    bbResult = "Kurang bobot";
                 } else if (imt <= 24.9) {
                     bbResult = "Normal";
                 } else if (imt <= 29.9) {
-                    bbResult = "kelebihan bobot";
+                    bbResult = "Kelebihan bobot";
                 } else if (imt <= 34.9) {
                     bbResult = "Obesitas kelas I";
                 } else if (imt <= 39.9) {
@@ -69,25 +121,42 @@ public class IMTCalculator extends JFrame {
                     bbResult = "Obesitas kelas III";
                 }
                 labelBBResult.setText("Kategori Bobot: " + bbResult);
+
+                if (imt <= 18.4) {
+                    categoryPanel.category = "BB Kurang";
+                    categoryPanel.color = Color.ORANGE;
+                } else if (imt <= 24.9) {
+                    categoryPanel.category = "Normal";
+                    categoryPanel.color = Color.GREEN;
+                } else {
+                    categoryPanel.category = "BB Berlebih";
+                    categoryPanel.color = Color.RED;
+                }
+
+                categoryPanel.repaint();
             }
         });
 
         box.add(labelAge);
         box.add(textFieldAge);
-        textFieldAge.setMargin(new Insets(0, 0, 0, 10));
+        textFieldAge.setMargin(new Insets(0, 0, 0, 8));
         box.add(labelHeight);
         box.add(textFieldHeight);
-        textFieldHeight.setMargin(new Insets(0, 0, 0, 10));
+        textFieldHeight.setMargin(new Insets(0, 0, 0, 8));
         box.add(labelWeight);
         box.add(textFieldWeight);
-        textFieldWeight.setMargin(new Insets(0, 0, 0, 10));
+        textFieldWeight.setMargin(new Insets(0, 0, 0, 8));
         box.add(labelGender);
         box.add(comboBoxGender);
         box.add(buttonCalculate);
         box.add(labelIMTResult);
         box.add(labelBBResult);
-        add(box);
 
+        JPanel centerPanel = new JPanel(new BorderLayout());
+        centerPanel.add(categoryPanel, BorderLayout.CENTER);
+        centerPanel.add(labelWeightCategories, BorderLayout.SOUTH);
+        add(box, BorderLayout.NORTH);
+        add(centerPanel, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
